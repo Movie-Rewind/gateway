d14fbf09302da6b6e905a79786f86dee297ce5b5
AlfonMnz <alfonmusan14@gmail.com>
2023-01-13 22:29:28 +0100

    Removing token validator

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const tokenValidator = require('./infrastructure/http/middleware/token-validator
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(tokenValidator);
+// app.use(tokenValidator);
 
 app.listen(port, () => {
   console.log(`Gateway listening on port ${port}`);
